5ecbe287e99f9cc8d69614ed7da6c0966557afc4
gooin <gooin@outlook.com>
2023-10-09 18:13:49 +0800

    fix: make old evn happy

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
+import * as fs from 'fs';
 
 export const checkIsDirectory = (filePath: string): boolean => {
     return fs.existsSync(filePath) && fs.lstatSync(filePath).isDirectory();
